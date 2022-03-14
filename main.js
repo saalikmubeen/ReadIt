4bffdff6ae76c3a71c61bf8df03c7d348dab7c7f
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-03-14 12:53:35 +0530

    configured electron window state

---
+++ main.js
@@ -0,0 +1,56 @@
+// Modules
+const {app, BrowserWindow} = require('electron')
+const windowStateKeeper = require("electron-window-state");
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow () {
+
+  let mainWindowState = windowStateKeeper({
+      defaultWidth: 1000,
+      defaultHeight: 800,
+  });
+
+  mainWindow = new BrowserWindow({
+    'x': mainWindowState.x,
+    'y': mainWindowState.y,
+    'width': mainWindowState.width,
+    'height': mainWindowState.height,
+    webPreferences: {
+      // --- !! IMPORTANT !! ---
+      // Disable 'contextIsolation' to allow 'nodeIntegration'
+      // 'contextIsolation' defaults to "true" as from Electron v12
+      contextIsolation: false,
+      nodeIntegration: true
+    }
+  })
+
+  mainWindowState.manage(mainWindow);
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile('src/main.html')
+
+  // Open DevTools - Remove for PRODUCTION!
+  mainWindow.webContents.openDevTools();
+
+  // Listen for window being closed
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
+}
+
+// Electron `app` is ready
+app.on('ready', createWindow)
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
