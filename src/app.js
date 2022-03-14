eccbdb4b0ae9f64932be072974032aac5acc59c2
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-03-14 13:25:57 +0530

    added toggle modal

--- src/app.js
+++ src/app.js
@@ -2,4 +2,17 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-alert("Hello world")
+
+const showModal = document.getElementById("show-modal"),
+    closeModal = document.getElementById("close-modal"),
+    modal = document.getElementById("modal");
+
+
+showModal.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
